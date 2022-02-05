bc18f0e6105ea032495cb549340755b25f74e9eb
Timothy Lee <timothylee43@gmail.com>
2022-02-05 02:09:22 -0800

    finish day 19

---
+++ Day-19-Turtle Coordin/main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for i in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[i])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[i])
+    all_turtles.append(tim)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            winning_turtle = turtle.pencolor()
+            if winning_turtle == user_bet:
+                print("you won!")
+            else:
+                print("you lost")
+            is_race_on = False
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+screen.exitonclick()
