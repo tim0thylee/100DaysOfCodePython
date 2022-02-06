0cd40bb90a2dd8fd5f99f12e4b00ce05952922b8
Timothy Lee <timothylee43@gmail.com>
2022-02-06 00:58:01 -0800

    finish part 1

---
+++ Day-20-Snake-Game-Part1/snake.py
@@ -0,0 +1,46 @@
+from turtle import Screen, Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create()
+        self.head = self.segments[0]
+
+    def create(self):
+        for position in STARTING_POSITIONS:
+            square = Turtle()
+            square.shape("square")
+            square.color("white")
+            square.penup()
+            square.goto(position)
+            self.segments.append(square)
+
+    def move(self):
+
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
