e8b0357872e114a6885b548bb493f80d2e6c3fe8
Timothy Lee <timothylee43@gmail.com>
2022-02-26 01:50:10 -0800

    coplete day 25

--- Day-25-US-States-Project/main.py
+++ Day-25-US-States-Project/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -13,7 +14,29 @@ def get_mouse_click_coor(x, y):
 
 turtle.onscreenclick(get_mouse_click_coor)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+#convert user input to title case
 
-# keep screen open
-turtle.mainloop()
+guessed_states = []
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{guessed_states} the State", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+        guessed_states.append(answer_state)
+
